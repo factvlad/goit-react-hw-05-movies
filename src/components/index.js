113dc55aca751fb0fc3546b8596a0407512c70bd
factvlad <fact1@ukr.net>
2022-09-04 14:13:53 +0300

    up

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export { default as Home } from '../pages/Home/Home';
 export { default as Movies } from '../pages/Movies/Movies';
 export { default as Cast } from '../pages/Cast/Cast';
-export { default as Reviews } from '../pages/Reviews/Reviews';
+// export { default as Reviews } from '../pages/Reviews/Reviews';
 export { default as NotFound } from '../pages/NotFound/NotFound';
 export { default as Layout } from './Layout/Layout';
 export { default as MovieDetails } from '../pages/MovieDetails/MovieDetails';
