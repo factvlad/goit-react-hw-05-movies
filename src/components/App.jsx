34af0855ae9fe67a8b8c0c2a302c448e0dd3cd9d
factvlad <fact1@ukr.net>
2022-09-04 16:55:54 +0300

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,34 @@
 import { Loader } from "components";
 import { Suspense } from 'react';
-import Routering from "./Routering/Routering";
+import { Routes, Route, } from "react-router-dom";
+import { lazy } from 'react';
 
+const Layout = lazy(() => import('./Layout/Layout'))
+const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'))
+const NotFound = lazy(() => import('./NotFound/NotFound'))
+const Movies = lazy(() => import('../pages/Movies/Movies'))
+const Home = lazy(() => import('../pages/Home/Home'))
+const Cast = lazy(() => import('../pages/Cast/Cast'))
+const Reviews = lazy(() => import('../pages/Reviews/Reviews'))
 
 
 const App = () => {
   return (
     <div>
       <Suspense fallback={ <Loader /> }>
-        <Routering />
+        <div>
+          <Routes>
+            <Route path="/" element={ <Layout /> }>
+              <Route index element={ <Home /> } />
+              <Route path="/movies" element={ <Movies /> } />
+              <Route path="/movies/:id" element={ <MovieDetails /> }>
+                <Route path="cast" element={ <Cast /> } />
+                <Route path="reviews" element={ <Reviews /> } />
+              </Route>
+              <Route path="*" element={ <NotFound /> } />
+            </Route>
+          </Routes>
+        </div>
       </Suspense>
     </div>
   );
